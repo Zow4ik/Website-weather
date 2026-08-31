eed3f240975374839689b6103914ae4acf31ca42
Artem Pokidyshev <aksadamas1999@gmail.com>
2026-08-31 19:30:49 +0300

    add: component InfoToday

---
+++ src/components/InfoToday/InfoToday.jsx
@@ -0,0 +1,26 @@
+import styles from './infoToday.module.scss'
+
+const InfoToday = () => {
+  return (
+    <>
+      <img
+        alt=""
+        src=""
+        width="316"
+        height="316"
+        loading="lazy"
+        data-js-header-image-weather
+      />
+      <span
+        className={styles.temperature}
+        data-js-header-temperature>
+      </span>
+      <div className={styles.location}>
+        <span data-js-header-city></span>
+        <span data-js-header-today></span>
+      </div>
+    </>
+  )
+}
+
+export default InfoToday
