e2a8bb04e6b74f2ed7fb509073efa548079919d6
Artem Pokidyshev <aksadamas1999@gmail.com>
2026-08-31 18:00:27 +0300

    new Initial structure on React

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,28 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+
+  css: {
+    modules: {
+      generateScopedName:
+        mode === "development"
+          ? "[name]__[local]__[hash:base64:5]"
+          : "[hash:base64:6]",
+    },
+
+    devSourcemap: true
+  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src"),
     },
   },
 
-  css: {
-    devSourcemap: true
-  },
   build: {
     sourcemap: true
   },
-
-  base: '/Website-weather/',
-})
+}))
